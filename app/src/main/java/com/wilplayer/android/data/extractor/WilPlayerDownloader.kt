2b677d4e7975029f638f8010303b2495216cbadf
Wil Gomez <wilgomez1983@gmail.com>
2026-05-06 10:34:40 -0500

    Add cookie header to request in WilPlayerDownloader
    Added a cookie header to avoid reload page issue.

--- app/src/main/java/com/wilplayer/android/data/extractor/WilPlayerDownloader.kt
+++ app/src/main/java/com/wilplayer/android/data/extractor/WilPlayerDownloader.kt
@@ -16,6 +16,8 @@ class WilPlayerDownloader private constructor() : Downloader() {
             val request = chain.request().newBuilder()
                 .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
                 .header("Accept-Language", "en-US,en;q=0.9")
+                // Esta cookie evita la página "The page needs to be reloaded"
+                .header("Cookie", "CONSENT=YES+")
                 .build()
             chain.proceed(request)
         }
