44f224146f6ff579581f42324e6f7af2a93a5024
Wil Gomez <wilgomez1983@gmail.com>
2026-05-06 13:46:44 -0500

    Add material3 components to SearchScreen

--- app/src/main/java/com/wilplayer/android/ui/screens/SearchScreen.kt
+++ app/src/main/java/com/wilplayer/android/ui/screens/SearchScreen.kt
@@ -1,5 +1,9 @@
 package com.wilplayer.android.ui.screens
 
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.*
